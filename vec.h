49d1c34ab35225f54b3bc76741056a9470322cc7
Tomasz Kramkowski <tk@the-tk.com>
2015-02-19 18:13:52 +0000

    Removed SIZEOF macro.

--- vec.h
+++ vec.h
@@ -23,9 +23,7 @@ float *vec_addm(float * const, float * const, const float);
 
 #define VEC_HEADER(vector) ((struct vec_header *)(vector - 1))
 
-#define SIZEOF(array) (sizeof(array) / sizeof(typeof(array[0])))
-
-#define vec(...) vec_new(SIZEOF( ((float []){ __VA_ARGS__ }) ), (float []){ __VA_ARGS__ })
+#define vec(...) vec_new(sizeof((float []){ __VA_ARGS__ }) / sizeof(float), (float []){ __VA_ARGS__ })
 
 #define __VEC_H
 #endif
